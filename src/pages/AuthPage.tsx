cc6d9317381a28d187ecc78cca5d4c6d18564987
elygo <el.uzb93@gmail.com>
2022-02-03 22:07:48 +0500

    feat: add authpage

---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/App.css'
+
+function Authpage() {
+  return (
+    <div className="Auth">
+      <div className="Sidebar">
+        Photo
+      </div>
+      <div className="Main">
+        Welcome
+      </div>
+    </div>
+  )
+}
+
+export default Authpage
