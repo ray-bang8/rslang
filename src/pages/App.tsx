b87ba022effa25fb8e1592c287347b6e9a5b987c
Shamshod <abdushamshod@gmail.com>
2022-02-06 01:42:36 +0500

    feat: add ebook page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/App.css'
 import AboutTeamPage from './AboutTeamPage'
+import EBookPage from './EBookPage'
 
 function App() {
   return (
     <div className="App">
-      <AboutTeamPage />
+      <EBookPage />
     </div>
   )
 }
