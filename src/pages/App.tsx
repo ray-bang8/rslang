f3964a3192c38a0d94390ec9b29e5b7032132686
elygo <el.uzb93@gmail.com>
2022-02-07 22:49:42 +0500

    fix: conflict after pull

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import '../styles/App.css'
 // import AboutTeamPage from './AboutTeamPage'
+// import AuthPage from './AuthPage'
 import DiffWordsPage from './DiffWordsPage'
-import AuthPage from './AuthPage'
 
 function App() {
   return (
     <div className="App">
       {/* <AboutTeamPage /> */}
       <DiffWordsPage />
-      <AuthPage />
+      {/* <AuthPage /> */}
     </div>
   )
 }
