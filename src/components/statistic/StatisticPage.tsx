ac4ea284626e1b2aefb0cd7de029841b20064694
ray-bang8 <kanatkyzy98@gmail.com>
2022-02-19 23:02:05 +0600

    init:create layot

---
+++ src/components/statistic/StatisticPage.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function StatisticPage() {
+  return (
+    <div className="stat-page">
+      <h3 className="stat-page__header">Statistic for today</h3>
+      <div className="stat-table">
+        <table className="stat-table__body table">
+          <thead className="stat-table__head">
+            <tr className="stat-table__row">
+              <th className="row__head">Game</th>
+              <th className="row__head">Learned words</th>
+              <th className="row__head">Correct(%)</th>
+              <th className="row__head">The longest streak</th>
+            </tr>
+          </thead>
+          <tbody className="stat-table__field">
+            <tr className="field__row">
+              <th className="field__row-root">Audio challenge</th>
+              <td className="field__row-root data">0</td>
+              <td className="field__row-root data">0%</td>
+              <td className="field__row-root data">0</td>
+            </tr>
+            <tr className="field__row">
+              <th className="field__row-root">Savanna</th>
+              <td className="field__row-root data">0</td>
+              <td className="field__row-root data">0%</td>
+              <td className="field__row-root data">0</td>
+            </tr>
+            <tr className="field__row">
+              <th className="field__row-root">My game</th>
+              <td className="field__row-root data">0</td>
+              <td className="field__row-root data">0%</td>
+              <td className="field__row-root data">0</td>
+            </tr>
+            <tr className="field__row last">
+              <th className="field__row-root">Total</th>
+              <td className="field__row-root data">0</td>
+              <td className="field__row-root data">0%</td>
+              <td className="field__row-root data" />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import '../styles/App.css'
+import StatisticPage from '../components/statistic/StatisticPage'
 // import AboutTeamPage from './AboutTeamPage'
 // import EBookPage from './EBookPage'
 import SprintGamePage from './SprintGamePage'
 // import AuthPage from './AuthPage'
 // import MainPage from './MainPag
+import '../styles/mainPage.scss'
 
 function App() {
   return (
     <div className="App">
       {/* <AboutTeamPage /> */}
-      <SprintGamePage />
-      {/* <AuthPage /> */}
+      {/* <SprintGamePage /> */}
+      <StatisticPage />
       {/* <MainPage /> */}
       {/* <EBookPage /> */}
     </div>
