bbca4905434871a8b77e569fed2eb65521e3553f
elygo <el.uzb93@gmail.com>
2022-02-21 02:28:21 +0500

    feat: final result of audicall

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,29 +1,21 @@
 import React from 'react'
 import '../styles/App.css'
 // import AboutTeamPage from './AboutTeamPage'
-<<<<<<< HEAD
-// import AuthPage from './AuthPage'
-import AudioCallsPage from './AudioCallsPage'
-=======
 // import EBookPage from './EBookPage'
-import SprintGamePage from './SprintGamePage'
+// import SprintGamePage from './SprintGamePage'
+import AudioCallsPage from './AudioCallsPage'
 // import AuthPage from './AuthPage'
 // import MainPage from './MainPag
->>>>>>> b2cdf6d3da8016aeb1e0edfc52c59cc7346c0e7a
 
 function App() {
   return (
     <div className="App">
       {/* <AboutTeamPage /> */}
-<<<<<<< HEAD
-      {/* <AuthPage /> */}
+      {/* <SprintGamePage /> */}
       <AudioCallsPage />
-=======
-      <SprintGamePage />
       {/* <AuthPage /> */}
       {/* <MainPage /> */}
       {/* <EBookPage /> */}
->>>>>>> b2cdf6d3da8016aeb1e0edfc52c59cc7346c0e7a
     </div>
   )
 }
