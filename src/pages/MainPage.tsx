d569c9c3c6302e60d0842c35e3db712098963239
ray-bang8 <kanatkyzy98@gmail.com>
2022-02-23 19:12:57 +0600

    fix

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,7 +6,7 @@ import MainSection from '../components/main-page/MainSection'
 import StatisticSection from '../components/main-page/StatisticSection'
 
 function MainPage() {
-  console.log('проверьте пожалуйста чуть позже,доделываю проект')
+  console.log('Мы по ошибке засабмитили ссылку на деплой,вот ссылка на PR https://github.com/ray-bang8/rslang/pull/38 , самооценка 500/600.Еще дорабатываем,можете оставить свой ник пожалуйста если проверяете')
   return (
     <main>
       <MainSection />
